819ff0246c360360f8fd8538b1f88824db2478ab
er-eis <eeisenberg0@gmail.com>
2021-02-02 22:12:32 -0500

    Base algorithm file

---
+++ pcreode/algorithm.py
@@ -0,0 +1,17 @@
+def down_sampling(data):
+    return
+
+def graph_construction(data):
+    return
+
+def end_state_identification(data):
+    return
+
+def hierarchical_placement(data):
+    return
+
+def consensus_alignment(data):
+    return
+
+def scoring(data):
+    return
